4f52188c1fc6d6fb175f9dd910cfb537da94ba6d
gypelayo <gypelayo@gmail.com>
2024-10-29 13:57:38 +0000

    Add cool icon

--- activity_logger/lib/models/activity.dart
+++ activity_logger/lib/models/activity.dart
@@ -1,15 +1,15 @@
 class Activity {
-  String name;
-  String type;
-  DateTime timestamp;
+  final String name;
+  final String type;
+  final DateTime timestamp;
   List<TimeEntry> timeEntries;
 
   Activity({
     required this.name,
     required this.type,
     required this.timestamp,
-    this.timeEntries = const [],
-  });
+    List<TimeEntry>? timeEntries,
+  }) : timeEntries = timeEntries ?? []; // Initializes timeEntries as an empty list if null
 
   Map<String, dynamic> toJson() => {
         'name': name,
@@ -22,11 +22,12 @@ class Activity {
         name: json['name'],
         type: json['type'],
         timestamp: DateTime.parse(json['timestamp']),
-        timeEntries: (json['timeEntries'] as List<dynamic>)
-            .map((e) => TimeEntry.fromJson(e as Map<String, dynamic>))
+        timeEntries: (json['timeEntries'] as List)
+            .map((e) => TimeEntry.fromJson(e))
             .toList(),
       );
 }
+
 
 class TimeEntry {
   DateTime date;
